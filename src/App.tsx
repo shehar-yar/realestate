a3b851f0755b88ca004ca8e60680da626ff8fda6
shehar-yar <mshaheerkhan013@gmail.com>
2024-12-08 20:17:48 +0500

    shad-cs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import './App.css'
 
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1><a href="#">Home Page</a></h1>
+      <Button className=" bg-black text-white">Home Button</Button>
     </>
   )
 }
